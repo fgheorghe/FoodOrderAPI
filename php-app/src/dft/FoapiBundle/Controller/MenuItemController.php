aa02a7d9e8e38184d85dc2a68cc68931b9ec84a5
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-11-11 20:40:08 +0000

    Added filtering.

--- php-app/src/dft/FoapiBundle/Controller/MenuItemController.php
+++ php-app/src/dft/FoapiBundle/Controller/MenuItemController.php
@@ -14,12 +14,20 @@ class MenuItemController extends Controller
 
     public function listAction()
     {
+        // _GET values.
+        $query = $this->container->get("request")->query;
         // Get the menu item service, that provides relevant logic.
         $menuItemService = $this->container->get('dft_foapi.menu_item');
 
         return $this->render('dftFoapiBundle:Common:data.json.twig', array(
                 "data" => $menuItemService->fetchAll(
-                        $this->container->get('dft_foapi.login')->getAuthenticatedUserId()
+                        $this->container->get('dft_foapi.login')->getAuthenticatedUserId(),
+                        array(
+                            "category_id" => $query->get('category_id'),
+                            "item_name" => $query->get('item_name'),
+                            "start" => $query->get('start'),
+                            "limit" => $query->get('limit')
+                        )
                  )
             )
         );

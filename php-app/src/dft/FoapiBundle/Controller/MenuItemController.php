90d162f76a45c4c6e2d4002b6625ab06ed25d005
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-11-11 19:48:34 +0000

    Do not html escape JSON responses. Cleaned for selecting menu items - and count. TODO: Apply filters.

--- php-app/src/dft/FoapiBundle/Controller/MenuItemController.php
+++ php-app/src/dft/FoapiBundle/Controller/MenuItemController.php
@@ -11,4 +11,17 @@ class MenuItemController extends Controller
         // TODO: Implement.
         return $this->render('dftFoapiBundle:Common:success.json.twig');
     }
+
+    public function listAction()
+    {
+        // Get the menu item service, that provides relevant logic.
+        $menuItemService = $this->container->get('dft_foapi.menu_item');
+
+        return $this->render('dftFoapiBundle:Common:data.json.twig', array(
+                "data" => $menuItemService->fetchAll(
+                        $this->container->get('dft_foapi.login')->getAuthenticatedUserId()
+                 )
+            )
+        );
+    }
 }

65d8fd43df4e275438ca4aa1e404f450ffbd0244
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-11-12 20:42:58 +0000

    Added user service.

--- php-app/src/dft/FoapiBundle/Controller/UsersController.php
+++ php-app/src/dft/FoapiBundle/Controller/UsersController.php
@@ -8,7 +8,20 @@ class UsersController extends Controller
 {
     public function listAction()
     {
-        // TODO: Implement.
-        return $this->render('dftFoapiBundle:Common:success.json.twig');
+        // _GET values.
+        $query = $this->container->get("request")->query;
+        // Get the user service.
+        $userService = $this->container->get('dft_foapi.user');
+
+        return $this->render('dftFoapiBundle:Common:data.json.twig', array(
+                "data" => $userService->fetchAll(
+                        $this->container->get('dft_foapi.login')->getAuthenticatedUserId(),
+                        array(
+                            "start" => $query->get('start'),
+                            "limit" => $query->get('limit')
+                        )
+                    )
+            )
+        );
     }
 }

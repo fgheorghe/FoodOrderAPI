d56f782cf42f11c96e62d456ef08d05a5bfd1b06
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-11-22 11:36:13 +0000

    Renamed index file. Added 'ehlo' route, name inspired by the SMTP ehlo verb. This can be used to tell if logged in or not - if not logged in, the authentication listener would not load it.

--- php-app/src/dft/FoapiBundle/Controller/DefaultController.php
+++ php-app/src/dft/FoapiBundle/Controller/DefaultController.php
@@ -9,6 +9,11 @@ class DefaultController extends Controller
     public function indexAction()
     {
         // TODO: Move version to a config variable.
-        return $this->render('dftFoapiBundle:Default:index.html.twig', array( "version" => "0.1" ));
+        return $this->render('dftFoapiBundle:Default:index.json.twig', array( "version" => "0.1" ));
+    }
+
+    public function ehloAction() {
+        // Tells the user they are logged in.
+        return $this->render('dftFoapiBundle:Default:ehlo.json.twig');
     }
 }

60e5d2f9836dd1a91d678a64432c9cb839c034a2
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-11-22 21:04:34 +0000

    Disabled IP checks for dev.

--- php-app/web/app_dev.php
+++ php-app/web/app_dev.php
@@ -13,8 +13,11 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '192.168.1.150', '192.168.0.100', 'fe80::1', '::1'))
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    // Disable this check for now!
+    if (false) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    }
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';

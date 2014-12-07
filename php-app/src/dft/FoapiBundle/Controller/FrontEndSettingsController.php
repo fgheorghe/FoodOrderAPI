2dd87f5038fd83f9fb637f89b52a9baab836f7a1
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-12-07 14:23:48 +0000

    Added logic for saving and retrieving f/e settings.

---
+++ php-app/src/dft/FoapiBundle/Controller/FrontEndSettingsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace dft\FoapiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FrontEndSettingsController extends Controller
+{
+    public function getAction()
+    {
+        // Get the front end service.
+        $frontEndSettingsService = $this->container->get('dft_foapi.front_end_settings');
+
+        return $this->render('dftFoapiBundle:Common:data.json.twig', array(
+                "data" => $frontEndSettingsService->fetchOne(
+                    $this->container->get('dft_foapi.login')->getAuthenticatedUserId()
+                )
+            )
+        );
+    }
+
+    // TODO: Return proper status code if failed.
+    // TODO: Allow for multiple branches.
+    // TODO: Verify delivery range data type.
+    public function updateAction()
+    {
+        // _POST values.
+        $request = $this->container->get("request");
+
+        // Get the restaurant service.
+        $frontEndSettingsService = $this->container->get('dft_foapi.front_end_settings');
+
+        // Update settings.
+        $frontEndSettingsService->updateFrontEndSettings(
+            $this->container->get('dft_foapi.login')->getAuthenticatedUserId(),
+            $request->get('restaurant_description'),
+            $request->get('phone_numbers'),
+            $request->get('facebook_page_url'),
+            $request->get('google_page_url'),
+            $request->get('twitter_page_url')
+        );
+
+        return $this->render('dftFoapiBundle:Common:success.json.twig');
+    }
+}
---
+++ php-app/src/dft/FoapiBundle/Services/FrontEndSettings.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fgheorghe
+ * Date: 18/11/14
+ * Time: 19:27
+ */
+
+namespace dft\FoapiBundle\Services;
+
+use dft\FoapiBundle\Traits\ContainerAware;
+use dft\FoapiBundle\Traits\Database;
+use dft\FoapiBundle\Traits\Logger;
+
+
+/**
+ * Class FrontEndSettings.
+ * NOTE: This class only updates values, does not create (REPLACE INTO).
+ * @package dft\FoapiBundle\Services
+ */
+class FrontEndSettings
+{
+    use ContainerAware;
+    use Database;
+    use Logger;
+
+    /**
+     * Method used for fetching front end settings.
+     * @param $userId
+     * @return null
+     */
+    public function fetchOne($userId)
+    {
+        return $this->executeFetchOneStatement($userId);
+    }
+
+    // Convenience method used for constructing the select query.
+    private function constructFetchOneSqlStatement()
+    {
+        return "SELECT
+                restaurant_description,
+                phone_numbers,
+                facebook_page_url,
+                google_page_url,
+                twitter_page_url
+                FROM front_end WHERE user_id = ? LIMIT 1";
+    }
+
+    // Method used for executing query.
+    private function executeFetchOneStatement($userId)
+    {
+        $query = $this->constructFetchOneSqlStatement();
+
+        // Prepare statement.
+        $statement = $this->prepare($query);
+        $statement->bindValue(1, $userId);
+
+        $statement->execute();
+        $results = $statement->fetchAll();
+
+        return count($results) == 1 ? $results[0] : null;
+    }
+
+    /**
+     * Updates front end settings.
+     * @param $userId
+     * @param $restaurantDescription
+     * @param $phoneNumbers
+     * @param $facebookPageUrl
+     * @param $googlePageUrl
+     * @param $twitterPageUrl
+     */
+    public function updateFrontEndSettings($userId, $restaurantDescription, $phoneNumbers, $facebookPageUrl,
+        $googlePageUrl, $twitterPageUrl) {
+        // Prepare query.
+        $query = "REPLACE INTO front_end SET
+            restaurant_description = ?,
+            phone_numbers = ?,
+            facebook_page_url = ?,
+            google_page_url = ?,
+            twitter_page_url = ?,
+            user_id = ?";
+
+        // Prepare statement.
+        $statement = $this->prepare($query);
+
+        // Bind parameters.
+        $statement->bindValue(1, $restaurantDescription);
+        $statement->bindValue(2, $phoneNumbers);
+        $statement->bindValue(3, $facebookPageUrl);
+        $statement->bindValue(4, $googlePageUrl);
+        $statement->bindValue(5, $twitterPageUrl);
+        $statement->bindValue(6, $userId);
+
+        // Execute.
+        $statement->execute();
+    }
+}

06efe2287fc290ae27f8d3221958309a0fe2dae1
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-12-26 15:18:04 +0000

    Added parameter for selecting only menu item categories with menu items.

--- php-app/src/dft/FoapiBundle/Controller/MenuItemCategoriesController.php
+++ php-app/src/dft/FoapiBundle/Controller/MenuItemCategoriesController.php
@@ -2,17 +2,24 @@
 
 namespace dft\FoapiBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class MenuItemCategoriesController extends Controller
+class MenuItemCategoriesController extends BaseController
 {
     public function listAction()
     {
+        // _GET values.
+        $query = $this->container->get("request")->query;
+
         // Get the menu item categories service.
         $menuItemCategoriesService = $this->container->get('dft_foapi.menu_item_categories');
 
         return $this->render('dftFoapiBundle:Common:data.json.twig', array(
-                "data" => $menuItemCategoriesService->fetchAll()
+                "data" => $menuItemCategoriesService->fetchAll(
+                        $this->getAuthenticatedUserIdAndSubAccountIds(),
+                        array(
+                            // Optional non_empty parameter.
+                            "non_empty" => $query->get('non_empty')
+                        )
+                    )
             )
         );
     }
--- php-app/src/dft/FoapiBundle/Services/MenuItemCategories.php
+++ php-app/src/dft/FoapiBundle/Services/MenuItemCategories.php
@@ -20,17 +20,49 @@ class MenuItemCategories {
 
     /**
      * Method used for fetching all menu item categories.
+     * @param $userId Array Although menu item categories are the same for all users,
+     * filters will require this parameter.
+     * @param $filters Array Optional filters. Most will require a valid user id. An
+     * example is non_empty - if set to 1, it will only return menu item categories
+     * with menu items. If this parameter is set, the service will include an item_count
+     * in the response object.
+     * @return Array
      */
-    public function fetchAll() {
-        return $this->executeFetchAllStatement();
+    public function fetchAll($userId, $filters = array()) {
+        return $this->executeFetchAllStatement($userId, $filters);
     }
 
     // Method used for executing query.
-    private function executeFetchAllStatement() {
-        $query = "SELECT * FROM menu_item_categories ORDER BY category_name ASC";
+    private function executeFetchAllStatement($userId, $filters) {
+        // Prepare base SQL statement.
+        $query = "SELECT * FROM menu_item_categories";
+
+        // Apply 'non_empty' filter.
+        if (array_key_exists("non_empty", $filters) && !is_null($filters["non_empty"]) && $filters["non_empty"] == 1) {
+            $query = "SELECT
+                    *,
+                    (SELECT
+                        COUNT(*)
+                    FROM
+                        menu_items
+                    WHERE
+                        menu_items.category_id = menu_item_categories.id
+                        AND menu_items.user_id IN (?)
+                    ) AS item_count
+                FROM menu_item_categories
+                HAVING item_count > 0";
+        }
+
+        // Apply sorting.
+        $query = $query . " ORDER BY category_name ASC";
 
         // Prepare statement.
         $statement = $this->prepare($query);
+
+        // Apply 'non_empty' filter.
+        if (array_key_exists('non_empty', $filters) && !is_null($filters["non_empty"]) && $filters["non_empty"] == 1) {
+            $statement->bindValue(1, $this->constructUserIdsIn($userId));
+        }
 
         $statement->execute();
         $results = $statement->fetchAll();
